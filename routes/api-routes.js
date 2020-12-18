ea32075e08a09263ed390257e8d57620d3997757
brendonstahl97 <brendonstahl97@gmail.com>
2020-12-17 19:10:26 -0500

    server will never generate room number that already exists

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,7 +1,7 @@
 const db = require("../models");
 const path = require('path');
 
-module.exports = function (app) {
+module.exports = function (app, games) {
   //serve html on / request
   app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/html/landing.html'));
@@ -9,7 +9,7 @@ module.exports = function (app) {
 
   app.get('/host', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/html/index.html'));
-    roomNum = Math.floor(Math.random() * 9999).toString();
+    roomNum = generateRoomNumber(games);
   });
 
   app.get('/join/:roomNumber', (req, res) => {
@@ -50,3 +50,26 @@ module.exports = function (app) {
     })
   })
 };
+
+
+const generateRoomNumber = (games) => {
+  roomNumber = Math.floor(Math.random() * 9999).toString();
+  if(checkRoomNum(roomNumber, games)) {
+    return roomNumber;
+  } else {
+    generateRoomNumber(games);
+  }
+};
+
+const checkRoomNum = (roomNum, games) => {
+  let isRoomGood = true;
+
+  games.forEach(game => {
+    if(game.room == roomNum) {
+      isRoomGood =  false;
+    };
+  });
+
+  return isRoomGood;
+
+}; 
